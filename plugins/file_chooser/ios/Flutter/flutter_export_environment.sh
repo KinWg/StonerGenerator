4dc5849273cafd0dda09af54126a9be19087929d
Kin <huntz@live.cn>
2020-08-11 17:42:13 +0800

    windows template

--- plugins/file_chooser/ios/Flutter/flutter_export_environment.sh
+++ plugins/file_chooser/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\UserData\flutter"
+export "FLUTTER_ROOT=C:\UserData\flutter-master\flutter"
 export "FLUTTER_APPLICATION_PATH=C:\UserData\FlutterProject\flutter-desktop-embedding\plugins\file_chooser"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=C:\UserData\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\UserData\flutter-master\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=0.2.0"
 export "FLUTTER_BUILD_NUMBER=0.2.0"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
